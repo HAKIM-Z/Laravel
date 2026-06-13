d80bd16c7a7fba30f32da8eccda6844c2fa2ddda
ABD ELHAKIM ZAHRA <101003540+HAKIM-Z@users.noreply.github.com>
2026-06-13 13:41:48 +0300

    commit

--- first-app/routes/web.php
+++ first-app/routes/web.php
@@ -6,10 +6,26 @@ Route::get('', function () {
     return view('welcome');
 });
 
-Route::get('home', function () {
-    return view('index');
-});
+
+
+// Route::get('home', function () {
+//     return view('index');
+// });
+
+Route::view('home', 'index');
 
 Route::get('hakim', function () {
     return "welcome hakim";
 });
+
+// Route::get('add', function () {
+//     return view('form');
+// });
+
+Route::view('add', 'form');
+
+Route::post('handel', function () {
+    return "inserted";
+});
+
+Route::redirect('x','home');
