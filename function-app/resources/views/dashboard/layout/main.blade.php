89ed4b91bf525c8b02bcead9fff32d61aa7d7ea2
ABD ELHAKIM ZAHRA <101003540+HAKIM-Z@users.noreply.github.com>
2026-06-19 19:45:45 +0300

    commit

---
+++ function-app/resources/views/dashboard/layout/main.blade.php
@@ -0,0 +1,3 @@
+@include('dashboard.layout.navbar');
+@include('dashboard.layout.sidebar');
+@include('dashboard.layout.footer');
---
+++ function-app/resources/views/dashboard/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('dashboard.layout.main');
+
+@section('body')
+
+<h1>index page</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, minima.</p>
+
+@endsection
