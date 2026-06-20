dffc70cf0cb16fff5fbe8c54056f8d3e6414ed98
ABD ELHAKIM ZAHRA <101003540+HAKIM-Z@users.noreply.github.com>
2026-06-20 15:09:49 +0300

    commit

---
+++ function-app/resources/views/dashboard/pages/admins/view.blade.php
@@ -0,0 +1,8 @@
+@extends('dashboard.layout.main')
+
+
+@section('body')
+
+<a class="btn btn-primary" href="{{route('admin.create')}}">Add Admin</a>
+
+@endsection
--- function-app/routes/admin.php
+++ function-app/routes/admin.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('index', function () {
     return view('dashboard.pages.index');
 });
+
+Route::resource('admin', AdminController::class);
